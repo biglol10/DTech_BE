31a4b1437d7dda61215bdf81e7d4038bcb6cea02
변지욱 <sgo1017099@mobmail.shinhan.com>
2022-06-22 17:33:30 +0900

    testing console.log

--- src/middleware/async.ts
+++ src/middleware/async.ts
@@ -2,7 +2,9 @@ import { AsyncHandler, GeneralTypeAsync } from '@src/util/commTypes';
 
 const asyncHandler: GeneralTypeAsync =
 	(fn: Function): AsyncHandler =>
-	(req, res, next) =>
+	(req, res, next) => {
+		console.log(req);
 		Promise.resolve(fn(req, res, next)).catch(next);
+	};
 
 export default asyncHandler;
--- src/util/commTypes.ts
+++ src/util/commTypes.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
-type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<any>;
+type AsyncHandler = (req: Request, res: Response, next: NextFunction) => any;
 
 type GeneralTypeAsync = (
 	fn: (req: Request, res: Response, next: NextFunction) => Promise<void>,
