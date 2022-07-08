24e2e47b1d978a3cde006db2435cb70214c0b60a
변지욱 <sgo1017099@mobmail.shinhan.com>
2022-07-08 14:47:55 +0900

    feature/JW/loginApi loginApi done

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,6 @@
 import asyncHandler from '@src/middleware/async';
 import queryExecutorResult from '@src/util/queryExecutorResult';
 import ErrorResponse from '@src/util/errorResponse';
-import UserModel from '@src/models/Usermodel';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import dayjs from 'dayjs';
@@ -22,30 +21,14 @@ export const registerUser = asyncHandler(async (req, res, next) => {
 
 	const hashedPassword = await bcrypt.hash(password, salt);
 
-	const sql = `INSERT INTO USER(USER_ID, NAME, PASSWD, TEAM_CD, TITLE, REGISTER_DATE, ADMIN) VALUES ('${userId}', '${name}', '${hashedPassword}', 'TMN0000002', '선임', CURRENT_TIMESTAMP,  0)`;
+	const sql = `INSERT INTO USER(USER_ID, NAME, PASSWD, TEAM_CD, TITLE, REGISTER_DATE, ADMIN) VALUES ('${userId}', '${name}', '${hashedPassword}', 'TMN0000002', '선임', SYSDATE(),  0)`;
 
 	const resultData = await queryExecutorResult(sql);
 
 	if (resultData.status === 'success' && process.env.JWT_SECRET) {
-		const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-			expiresIn: process.env.JWT_EXPIRE,
-		});
+		const { token, options } = tokenResponse(userId, process.env.JWT_SECRET);
 
-		const cookie_expire = process.env.COOKIE_EXPIRE
-			? parseInt(process.env.COOKIE_EXPIRE, 10)
-			: 30;
-
-		const options = {
-			expires: new Date(Date.now() + cookie_expire * 24 * 60 * 60 * 1000),
-			httpOnly: true, // only want the cookie to be access through client side script
-			secure: false,
-		};
-
-		if (process.env.NODE_ENV === 'production') {
-			options.secure = true; // https (production)
-		}
-
-		return res.status(200).cookie('token', token, options).json({
+		return res.status(200).cookie('user_token', token, options).json({
 			name,
 			userId,
 			time,
@@ -56,6 +39,8 @@ export const registerUser = asyncHandler(async (req, res, next) => {
 		return res.status(401).json({
 			result: 'fail',
 			message: 'User register failed',
+			status: resultData.status || 'err from node',
+			sqlMessage: resultData.sqlMessage || 'check env var',
 		});
 	}
 });
@@ -67,17 +52,64 @@ export const loginUser = asyncHandler(async (req, res, next) => {
 		return next(new ErrorResponse('아이디/비밀번호를 입력해주세요', 400));
 	}
 
-	const user = UserModel.filter((item) => item.USER_ID === userId && item.PASSWD === password);
+	const userFindSql = `SELECT * FROM USER WHERE USER_ID = '${userId}'`;
 
-	if (user && user.length > 0) {
-		return res.status(200).json({
-			success: true,
-			user,
+	const { queryResult: selectedUser } = await queryExecutorResult(userFindSql);
+
+	if (selectedUser.length === 0) {
+		return next(new ErrorResponse('로그인에 실패했습니다', 401));
+	}
+
+	const isMatch = await matchPassword(password, selectedUser[0].PASSWD);
+
+	if (!isMatch) {
+		return next(new ErrorResponse('로그인에 실패했습니다', 401));
+	}
+
+	const updateUserLoginSql = `UPDATE USER SET REGISTER_DATE = SYSDATE() WHERE USER_ID = '${userId}'`;
+
+	const { queryResult: updateResult } = await queryExecutorResult(updateUserLoginSql);
+
+	if (updateResult.affectedRows && process.env.JWT_SECRET) {
+		const { token, options } = tokenResponse(userId, process.env.JWT_SECRET);
+
+		const time = dayjs();
+
+		return res.status(200).cookie('user_token', token, options).json({
+			name: selectedUser.NAME,
+			userId,
+			time,
+			token,
+			result: 'success',
 		});
 	} else {
 		return res.status(401).json({
-			success: false,
-			user: null,
+			result: 'error',
+			message: 'User login failed',
 		});
 	}
 });
+
+const matchPassword = async (enteredPassword: string, dbPassword: string) => {
+	return await bcrypt.compare(enteredPassword, dbPassword);
+};
+
+const tokenResponse = (userId: string, jwt_secret: string) => {
+	const token = jwt.sign({ id: userId }, jwt_secret, {
+		expiresIn: process.env.JWT_EXPIRE,
+	});
+
+	const cookie_expire = process.env.COOKIE_EXPIRE ? parseInt(process.env.COOKIE_EXPIRE, 10) : 30;
+
+	const options = {
+		expires: new Date(Date.now() + cookie_expire * 24 * 60 * 60 * 1000),
+		httpOnly: true, // only want the cookie to be access through client side script
+		secure: false,
+	};
+
+	if (process.env.NODE_ENV === 'production') {
+		options.secure = true; // https (production)
+	}
+
+	return { token, options };
+};
